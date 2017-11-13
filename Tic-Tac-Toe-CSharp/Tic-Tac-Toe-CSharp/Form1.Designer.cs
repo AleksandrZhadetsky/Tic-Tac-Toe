09d0320a9ac284bc4c3ffed249afa98441b08bf4
Gary Robinson <grobinson@hornets.com>
2017-11-13 10:22:42 -0500

    Added New Game Functionality

--- Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Form1.Designer.cs
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Form1.Designer.cs
@@ -29,13 +29,6 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lblstatus = new System.Windows.Forms.Label();
-            this.btnNewButton = new System.Windows.Forms.Button();
-            this.player2 = new System.Windows.Forms.PictureBox();
-            this.player1 = new System.Windows.Forms.PictureBox();
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
@@ -45,9 +38,14 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox0 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.btnNewButton = new System.Windows.Forms.Button();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
@@ -57,6 +55,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -70,90 +70,18 @@
             this.groupBox1.Controls.Add(this.pictureBox2);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.pictureBox0);
-            this.groupBox1.Location = new System.Drawing.Point(114, 104);
+            this.groupBox1.Location = new System.Drawing.Point(143, 114);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(420, 399);
+            this.groupBox1.Size = new System.Drawing.Size(417, 401);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(8, 114);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Player1";
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(556, 114);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 23);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Player2";
-            // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(228, 45);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(188, 23);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "TIC TAC TOE";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // lblstatus
-            // 
-            this.lblstatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblstatus.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblstatus.Location = new System.Drawing.Point(181, 597);
-            this.lblstatus.Name = "lblstatus";
-            this.lblstatus.Size = new System.Drawing.Size(268, 33);
-            this.lblstatus.TabIndex = 4;
-            this.lblstatus.Text = "Status Goes Here";
-            this.lblstatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // btnNewButton
-            // 
-            this.btnNewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNewButton.Location = new System.Drawing.Point(181, 537);
-            this.btnNewButton.Name = "btnNewButton";
-            this.btnNewButton.Size = new System.Drawing.Size(268, 43);
-            this.btnNewButton.TabIndex = 5;
-            this.btnNewButton.Text = "New Game";
-            this.btnNewButton.UseVisualStyleBackColor = true;
-            // 
-            // player2
-            // 
-            this.player2.Image = global::Tic_Tac_Toe_CSharp.Properties.Resources.addias;
-            this.player2.Location = new System.Drawing.Point(559, 156);
-            this.player2.Name = "player2";
-            this.player2.Size = new System.Drawing.Size(97, 81);
-            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.player2.TabIndex = 8;
-            this.player2.TabStop = false;
-            // 
-            // player1
-            // 
-            this.player1.Image = global::Tic_Tac_Toe_CSharp.Properties.Resources.NikeRed;
-            this.player1.Location = new System.Drawing.Point(13, 156);
-            this.player1.Name = "player1";
-            this.player1.Size = new System.Drawing.Size(82, 72);
-            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.player1.TabIndex = 6;
-            this.player1.TabStop = false;
             // 
             // pictureBox8
             // 
             this.pictureBox8.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox8.Location = new System.Drawing.Point(254, 236);
+            this.pictureBox8.Location = new System.Drawing.Point(264, 244);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(96, 93);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -166,7 +94,7 @@
             this.pictureBox7.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox7.Location = new System.Drawing.Point(156, 236);
+            this.pictureBox7.Location = new System.Drawing.Point(166, 244);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(96, 93);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -179,7 +107,7 @@
             this.pictureBox6.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox6.Location = new System.Drawing.Point(58, 236);
+            this.pictureBox6.Location = new System.Drawing.Point(68, 244);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(96, 93);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -192,7 +120,7 @@
             this.pictureBox5.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox5.Location = new System.Drawing.Point(254, 145);
+            this.pictureBox5.Location = new System.Drawing.Point(264, 153);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(96, 93);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -205,7 +133,7 @@
             this.pictureBox4.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox4.Location = new System.Drawing.Point(156, 145);
+            this.pictureBox4.Location = new System.Drawing.Point(166, 153);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(96, 93);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -218,7 +146,7 @@
             this.pictureBox3.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox3.Location = new System.Drawing.Point(58, 145);
+            this.pictureBox3.Location = new System.Drawing.Point(68, 153);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(96, 93);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -231,7 +159,7 @@
             this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox2.Location = new System.Drawing.Point(254, 52);
+            this.pictureBox2.Location = new System.Drawing.Point(264, 60);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(96, 93);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -244,7 +172,7 @@
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Location = new System.Drawing.Point(156, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(166, 60);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(96, 93);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -257,7 +185,7 @@
             this.pictureBox0.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox0.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox0.Location = new System.Drawing.Point(58, 52);
+            this.pictureBox0.Location = new System.Drawing.Point(68, 60);
             this.pictureBox0.Name = "pictureBox0";
             this.pictureBox0.Size = new System.Drawing.Size(96, 93);
             this.pictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -265,11 +193,86 @@
             this.pictureBox0.TabStop = false;
             this.pictureBox0.Click += new System.EventHandler(this.OnClick);
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(23, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(581, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(142, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(404, 62);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TIC TAC TOE";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstatus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblstatus.Location = new System.Drawing.Point(155, 599);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(391, 47);
+            this.lblstatus.TabIndex = 4;
+            this.lblstatus.Text = "Status Goes Here";
+            this.lblstatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNewButton
+            // 
+            this.btnNewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewButton.Location = new System.Drawing.Point(211, 539);
+            this.btnNewButton.Name = "btnNewButton";
+            this.btnNewButton.Size = new System.Drawing.Size(292, 43);
+            this.btnNewButton.TabIndex = 5;
+            this.btnNewButton.Text = "New Game";
+            this.btnNewButton.UseVisualStyleBackColor = true;
+            this.btnNewButton.Click += new System.EventHandler(this.btnNewButton_Click);
+            // 
+            // player2
+            // 
+            this.player2.Image = global::Tic_Tac_Toe_CSharp.Properties.Resources.addias;
+            this.player2.Location = new System.Drawing.Point(584, 156);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(97, 81);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 8;
+            this.player2.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.Image = global::Tic_Tac_Toe_CSharp.Properties.Resources.NikeRed;
+            this.player1.Location = new System.Drawing.Point(26, 156);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(96, 81);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player1.TabIndex = 6;
+            this.player1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 639);
+            this.ClientSize = new System.Drawing.Size(703, 671);
             this.Controls.Add(this.player2);
             this.Controls.Add(this.player1);
             this.Controls.Add(this.btnNewButton);
@@ -285,8 +288,6 @@
             this.Text = "Tic Tac Toe";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
@@ -296,6 +297,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Form1.cs
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Form1.cs
@@ -19,8 +19,37 @@ namespace Tic_Tac_Toe_CSharp
 
         void OnNewGame()
         {
+            PictureBox[] allPictures = { pictureBox0,
+                                          pictureBox1,
+                                          pictureBox2,
+                                          pictureBox3,
+                                          pictureBox4,
+                                          pictureBox5,
+                                          pictureBox6,
+                                          pictureBox7,
+                                          pictureBox8,
+                                          };
+
+            //Clear all game board cells
+            foreach (var p in allPictures)
+            {
+                p.Image = null;
+            }
+
             turn = PlayerTurn.Player1;
+            
             lblstatus.Text = "";
+        }
+
+        void ShowTurn()
+        {
+            string status = "";
+            if (turn == PlayerTurn.Player1)
+                status = "Turn: Player 1";
+            else
+                status = "Turn: Player 2";
+
+            lblstatus.Text = status;
         }
 
         public Form1()
@@ -32,6 +61,8 @@ namespace Tic_Tac_Toe_CSharp
         {
             PictureBox p = sender as PictureBox;
 
+            if (p.Image != null)
+                return;
             if (turn == PlayerTurn.Player1)
             {
                 p.Image = player1.Image;
@@ -43,12 +74,29 @@ namespace Tic_Tac_Toe_CSharp
 
             //Change turns
             turn = (PlayerTurn.Player1 == turn) ? PlayerTurn.Player2 : PlayerTurn.Player1;
+
+            ShowTurn();
+
             
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             OnNewGame();
+        }
+
+        private void btnNewButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure you want to start a new game?",
+                                          "New Game",
+                                          MessageBoxButtons.YesNo,
+                                          MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                OnNewGame();
+            }
+                
         }
     }
 }
